92f3eaa443fb6bbbb4f648aad9b8172f585bbe7d
Sahasragoud <146707817+Sahasragoud@users.noreply.github.com>
2026-07-13 11:53:54 +0530

    Add files via upload

---
+++ Hands on 2/greet/src/test/java/com/cognizant/eureka_discovery_client/EurekaDiscoveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.eureka_discovery_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
